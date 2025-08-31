7e2d2e39817f7e290645ec33bca982b8104ffa23
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-08-31 21:02:58 +0530

    added to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/" element={<Signup />} />
       </Routes>
     </Router>

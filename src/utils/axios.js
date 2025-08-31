93cf8ea4bbd4064ddb96ac79bf2573f98aee4f25
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-08-31 17:09:03 +0530

    added to test

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/auth", // change in production
+  baseURL: "https://server-test-8stv.onrender.com/api/auth", // change in production
 });
 
 export default instance;

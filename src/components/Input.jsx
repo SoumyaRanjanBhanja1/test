c9b88954cc156968dc3b4aa2d9a3a2214ee0c7b9
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-08-31 20:23:13 +0530

    added to test

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,20 +1,15 @@
-const Input = ({ label, type, value, onChange, name, placeholder }) => {
+const Input = ({ label, type, value, onChange,name }) => {
   return (
-    <div className="relative">
+    <div className="mb-4 w-full">
+      <label className="block text-gray-700 text-sm font-bold mb-2">{label}</label>
       <input
         type={type}
-        name={name}
         value={value}
+        name={name}
         onChange={onChange}
+        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         required
-        placeholder={placeholder}
-        className="peer w-full px-4 py-3 bg-white/10 text-white placeholder-transparent border border-white/20 rounded-xl focus:outline-none focus:border-pink-400 focus:ring-2 focus:ring-pink-500/50 backdrop-blur-md transition-all"
       />
-      <label
-        className="absolute left-4 -top-2 text-xs text-pink-300 transition-all peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3 peer-placeholder-shown:text-sm peer-focus:-top-2 peer-focus:text-xs peer-focus:text-pink-300 bg-indigo-900/60 px-1 rounded"
-      >
-        {label}
-      </label>
     </div>
   );
 };
